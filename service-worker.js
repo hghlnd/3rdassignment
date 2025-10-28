0242cc1d54394f031bde7c4846002cc32beffb17
hghlnd <61725271+hghlnd@users.noreply.github.com>
2025-10-27 23:34:02 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -10,27 +10,20 @@ const FILES_TO_CACHE = [
   './icon-512x512.png'
 ];
 
-// Install event - cache important files
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
   );
-  console.log('Service Worker installed.');
 });
 
-// Activate event
 self.addEventListener('activate', (event) => {
   event.waitUntil(self.clients.claim());
-  console.log('Service Worker activated.');
 });
 
-// Fetch event - serve cached content when offline
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      return cachedResponse || fetch(event.request).catch(() => caches.match('./index.html'));
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request).catch(() => caches.match('./index.html'));
     })
   );
 });
